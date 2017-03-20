c7b151be0c166cd33e7ce5f01e0ac945efea4440
Yada Pruksachatkun <yadapruk@mit.edu>
2017-03-20 16:29:13 -0400

    almost perfect - 0.98 accuracy for both

--- fraud.py
+++ fraud.py
@@ -19,9 +19,10 @@ import time
 3. Use others, like random forest
 
 """
+NUM = 400
 def extract_features():
 	scaler = StandardScaler()
-	X_train_pos = pd.DataFrame(columns=[  'lastSRSCount', 'snooze', 'num_seats'])
+	X_train_pos = pd.DataFrame(columns=[  'lastSRSCount', 'snooze', 'num_seats', ])
 	X_train_neg =  pd.DataFrame(columns=[  'lastSRSCount', 'snooze', 'num_seats'])
 	X_test_neg =  pd.DataFrame(columns=[  'lastSRSCount', 'snooze', 'num_seats'])
 	X_test_pos =   pd.DataFrame(columns=[  'lastSRSCount', 'snooze', 'num_seats'])
@@ -35,33 +36,23 @@ def extract_features():
 		index = 1
 		snaps = pd.read_csv("snapshots.csv") 
 		for row in reader:
-			print("row")
 			if index == 1:
 				index += 1
-			else:
+			elif index < NUM:
 				banned = row[-1]
 				curr_id = row[0]
-				SRS_string = ''
-				ind = 8
-				SRS_string = ''
-				ind = 8
-				while "]" not in row[ind]:
-					SRS_string += row[ind] 
-					ind += 1
-				#print("done")
-				SRS_string += row[ind]
-				SRS_string = SRS_string.replace("\"\"\"", "\",")
-				#print(SRS_string)
-				SRS_string = ast.literal_eval(SRS_string)
 				if (row[7] == "false"):
 					sn = 0
 				else:
 					sn = 1
 
-				snapsh = snaps.loc[snaps['userId'] == curr_id]		
+				snapsh = snaps.loc[snaps['userId'] == curr_id]	
+
 				curr = { 'lastSRSCount': row[6], 'snooze': sn, 'num_seats': len(snapsh) }
 				# Making sure the number of 
-				if random.random() > 0.5:
+				# MAKE SURE THERE IS A ONE HERE. 
+				rand = random.random()
+				if rand > 0.5:
 					if (banned == "true"):
 						X_train_pos = X_train_pos.append(curr, ignore_index=True)
 						Y_train_pos = Y_train_pos.append({'res': 1},ignore_index=True )
@@ -75,11 +66,43 @@ def extract_features():
 					else:
 						X_test_neg = X_test_neg.append(curr,ignore_index=True)
 						Y_test_neg = Y_test_neg.append({'res': 0},ignore_index=True )
-				X_train_neg = X_train_neg[:len(X_train_pos)] # half neg, half pos examples
-				Y_train_neg = Y_train_neg[:len(X_train_pos)]
-				X_train = X_train_neg.append(X_train_pos)
-				Y_train = Y_train_neg.append(Y_train_pos)
-		return X_train, Y_train, X_test_neg, Y_test_neg, X_test_pos, Y_test_pos
+				index += 1
+	event = pd.read_csv("eventusers.csv") 
+	eventh = event.loc[event['banned'] == True]
+	if (len( Y_train_pos) < len(Y_train_neg)):
+		num_fill = len(Y_train_neg) - len( Y_train_pos) 
+		eventha = eventh.head(num_fill)
+		for index, row in eventha.iterrows():
+			banned = row['banned']
+			curr_id = row['_id']
+			if (row[7] == 'False'):
+				sn = 0
+			else:
+				sn = 1
+			snapsh = snaps.loc[snaps['userId'] == curr_id]	
+			curr = { 'lastSRSCount': row['lastSRSCount'], 'snooze': sn, 'num_seats': len(snapsh) }
+			X_train_pos = X_train_pos.append(curr, ignore_index=True)
+			Y_train_pos = Y_train_pos.append({'res': 1},ignore_index=True )
+	if (len(Y_test_pos) < len(Y_test_neg)):
+		num_fill = len(Y_test_neg) - len( Y_test_pos) 
+		eventhb = eventh.head(num_fill)
+		for index, row in eventhb.iterrows():
+			banned = row['banned']
+			curr_id = row['_id']
+			if (row[7] == 'False'):
+				sn = 0
+			else:
+				sn = 1
+			snapsh = snaps.loc[snaps['userId'] == curr_id]	
+			curr = { 'lastSRSCount': row['lastSRSCount'], 'snooze': sn, 'num_seats': len(snapsh) }
+			X_test_pos = X_test_pos.append(curr, ignore_index=True)
+			Y_test_pos = Y_test_pos.append({'res': 1},ignore_index=True )
+
+	X_train_neg = X_train_neg[:len(X_train_pos)] # half neg, half pos examples
+	Y_train_neg = Y_train_neg[:len(X_train_pos)]
+	X_train = X_train_neg.append(X_train_pos)
+	Y_train = Y_train_neg.append(Y_train_pos)
+	return X_train, Y_train, X_test_neg, Y_test_neg, X_test_pos, Y_test_pos
 
 
 
@@ -103,17 +126,27 @@ def test(classifier_funcs, X_test, Y_test):
 
 
 X_train, Y_train, X_test_neg, Y_test_neg, X_test_pos, Y_test_pos = extract_features() 
-print(X_train)
+print("TEST")
+#print(X_train)
 print(len(X_train))
+#print('y train')
 print(len(Y_train))
+#print(Y_train)
+print("X Test neg")
+#print(X_test_neg)
+print("Y test neg")
+print("x test pos")
+print(len(X_test_pos))
+print("y test pos")
+print(len(Y_test_pos))
 print("seperate data")
 classifiers = []
 clsfr1 = nn_one(X_train, Y_train)
 print("training done")
 classifiers.append(clsfr1)
-print("false negatives, meaning it was a banned, and predicts not banned") 
+print("fPercentage of banned that are predicted") 
 test(classifiers, X_test_pos, Y_test_pos)
-print("false  positives- says banned, is not") 
+print("Percentage of not banned that were predicted correctly") 
 test(classifiers, X_test_neg, Y_test_neg)
 # Maybe I sould be having my onw thing. iT'S ALREAYD mRACH. 
 
