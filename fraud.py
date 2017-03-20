ca0977c24dfdd69ee10d1d7b7ebf5e5aad579eee
Yada Pruksachatkun <yadapruk@mit.edu>
2017-03-20 14:09:33 -0400

    first working model

--- fraud.py
+++ fraud.py
@@ -1,8 +1,11 @@
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report,confusion_matrix
 import csv
 import pandas as pd
 import random 
 import numpy as np
+import ast
 import time
 """ Goal: Detect fruad, where requesting changes in stadium > taking pictures < sharing on FB"""
 # Shindig at 5 pm on Saturday
@@ -17,111 +20,93 @@ import time
 
 """
 def extract_features():
-	X_train= []
-	Y_train = []
-	X_test_neg = []
-	X_test_pos = []
-	Y_test_pos = []
-	Y_test_neg = []
-	with open("eventusers.csv", "rb") as csvfile:
+	scaler = StandardScaler()
+	X_train_pos = pd.DataFrame(columns=[  'lastSRSCount', 'snooze'])
+	X_train_neg = pd.DataFrame(columns=[  'lastSRSCount', 'snooze'])
+	X_test_neg = pd.DataFrame(columns=[  'lastSRSCount', 'snooze'])
+	X_test_pos =  pd.DataFrame(columns=[ 'lastSRSCount', 'snooze'])
+	Y_test_neg =  pd.DataFrame()
+	Y_test_pos  = pd.DataFrame()
+	Y_train_pos = pd.DataFrame()
+	Y_train_neg = pd.DataFrame()
+
+	with open("eventusers.csv", "rt") as csvfile:
 		reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-		Fraud = [] # Here, tuple (id, users, number, et.c. )
-		next(reader, None)
-		snaps = pd.read_csv("snapshots.csv") 
+		index = 1
 		for row in reader:
-			print("Row")
-			# 3, 6, 8, 10 for banned
-			curr_id = row[3] 
-			SRS_seq = row[6]
-			i = 10
-			banned = row[-1]
-			#print(SRS_seq)
-			#print(SRS_num)
-			snaps = snaps.loc[snaps['userId'] == curr_id]
-			snap_id = []
-			seats = {}
-			for index, row in snaps.iterrows():
-				seat = row.iloc[0]
-				seat = seat.split("-")
-				seat_num = ""
-				to_compare = ""
-				for j in range(len(seat)):
-					if "cam" in seat[j]:
-						to_compare = seat[j+2] + seat[j+3]
-						seat_num = seat[j+1] + seat[j+2] + seat[j+3]
-					if (seats.get(seat_num)):
-						seats[seat_num] += 1
+			#rint(row)
+			if index == 1:
+				index += 1
+			elif index < 100:
+				print(row)
+				banned = row[-1]
+				SRS_string = ''
+				ind = 8
+				if (row[7] == "false"):
+					sn = 0
+				else:
+					sn = 1
+				curr = { 'lastSRSCount': row[6], 'snooze': sn }
+				if random.random() > 0.5:
+					if (banned == "true"):
+						X_train_pos = X_train_pos.append(curr, ignore_index=True)
+						Y_train_pos = Y_train_pos.append({'res': 1},ignore_index=True )
 					else:
-						seats[seat_num] = 1 
-				to_compare_with = row["row"] + str(row["seat"])
-				vector_final =  [len(seats)]
-
-				if (to_compare == to_compare_with):
-					vector_final.append(1) # True - same seat that tehy're in
+						X_train_neg = X_train_neg.append(curr, ignore_index=True)
+						Y_train_neg = Y_train_neg.append({'res': 0},ignore_index=True )
 				else:
-					vector_final.append(-1) 
-
-			### HERE, THIS IS WHERE WE PUT WEHAT WE HAVE EXTRACTED 
-			### INTO A STRING
-			
-			for k in seats.values():
-				vector_final.append(k)
-			# I'm here rn 
-			if random.random() > 0.5:
-				X_train.append(vector_final)
-
-				if (banned == "true"):
-					Y_train.append(1)
-				else:
-					Y_train.append(0)
-			else:
-				if (banned == "true"):
-					X_test_pos.append(vector_final)
-					Y_test_pos.append(1)
-				else:
-					X_test_neg.append(vector_final)
-					Y_test_neg.append(0)
-		return X_train, Y_train, X_test_neg, Y_test_neg, X_test_pos, Y_test_pos
+					if (banned == "true"):
+						X_test_pos = X_test_pos.append(curr, ignore_index=True)
+						Y_test_pos = Y_test_pos.append({'res': 1},ignore_index=True )
+					else:
+						X_test_neg = X_test_neg.append(curr,ignore_index=True)
+						Y_test_neg = Y_test_neg.append({'res': 0},ignore_index=True )
+			X_train_neg = X_train_neg[:len(X_train_pos)] # half neg, half pos examples
+			Y_train_neg = Y_train_neg[:len(X_train_pos)]
+			X_train = X_train_neg.append(X_train_pos)
+			Y_train = Y_train_neg.append(Y_train_pos)
+			index += 1
+		else:
+			return X_train, Y_train, X_test_neg, Y_test_neg, X_test_pos, Y_test_pos
 
 
 
 def nn_one(X_train, Y_train):
 	#X = [[0., [0., 0.]], [1., [0., 0.] ]]
 	#Y = [0,1]
+
 	clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-		hidden_layer_sizes=(5, 2), random_state=1)
+		hidden_layer_sizes=(30,30,30), random_state=1)
 	clf.fit(X_train, Y_train)
 	return clf
-
 
 def test(classifier_funcs, X_test, Y_test):
 	for c in classifier_funcs:
 		start = time.time()
-		predictions = np.array(c.predict(X_test))
-        end = time.time()
-        print("Time to predict in seconds")
-        print(end-start)
-        error = np.mean( predictions != Y_test)
-        print("Percentage wrong")
-        print(error)
-        print(predictions)
+		print(c.score(X_test, Y_test))
+		end = time.time()
+		print("Time to predict in seconds")
+		print(end-start)
 
 
-X_train, Y_train, X_test_neg, Y_test_neg, X_test_pos, Y_test_pos = extract_features()
+
+X_train, Y_train, X_test_neg, Y_test_neg, X_test_pos, Y_test_pos = extract_features() 
+print(X_train)
 #X_train = np.array(X_train).reshape((len(X_train), 1))
 #Y_train = np.array(Y_train).reshape((len(Y_train), 1))
 print(len(X_train))
 print(len(Y_train))
 print("seperate data")
 classifiers = []
+# foR x TRAIN, YOU CAN JUST PUT IT STRAIGHT IN. 
 clsfr1 = nn_one(X_train, Y_train)
 print("training done")
 classifiers.append(clsfr1)
-print("now testing")
 print("false negatives, meaning it was a banned, and predicts not banned") 
 test(classifiers, X_test_pos, Y_test_pos)
 print("false  positives- says banned, is not") 
 test(classifiers, X_test_neg, Y_test_neg)
+# Maybe I sould be having my onw thing. iT'S ALREAYD mRACH. 
 
 
 
